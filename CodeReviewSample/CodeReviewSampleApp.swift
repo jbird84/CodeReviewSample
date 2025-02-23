be3f5f8ad61a99bc31d6967ee4bb8cf222837d26
DoodMac <48062579+jbird84@users.noreply.github.com>
2025-02-23 10:44:43 -0600

    Initial Commit

---
+++ CodeReviewSample/CodeReviewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReviewSampleApp.swift
+//  CodeReviewSample
+//
+//  Created by Kinney Kare on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeReviewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeReviewSampleTests/CodeReviewSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReviewSampleTests.swift
+//  CodeReviewSampleTests
+//
+//  Created by Kinney Kare on 2/23/25.
+//
+
+import Testing
+@testable import CodeReviewSample
+
+struct CodeReviewSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
